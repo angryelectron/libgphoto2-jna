404fcbe870e8b34d351da674629cf47c47f6ba8a
abythell <abythell@ieee.org>
2013-01-28 15:58:12 -0800

    improved/implemented capture(), captureAndDownload(), burstAndDownload()

--- src/com/angryelectron/gphoto2/GPhoto2.java
+++ src/com/angryelectron/gphoto2/GPhoto2.java
@@ -27,6 +27,7 @@ import com.angryelectron.libgphoto2.Gphoto2Library.CameraCaptureType;
 import com.angryelectron.libgphoto2.Gphoto2Library.CameraEventType;
 import com.angryelectron.libgphoto2.Gphoto2Library.CameraFile;
 import com.angryelectron.libgphoto2.Gphoto2Library.CameraFileType;
+import com.angryelectron.libgphoto2.Gphoto2Library.CameraList;
 import com.angryelectron.libgphoto2.Gphoto2Library.GPContext;
 import com.angryelectron.libgphoto2.Gphoto2Library.GPContextErrorFunc;
 import com.angryelectron.libgphoto2.Gphoto2Library.GPContextMessageFunc;
@@ -37,8 +38,7 @@ import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.PointerByReference;
 import java.io.File;
 import java.io.IOException;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import java.util.ArrayList;
 
 /**
  * Simple API for controlling a camera using libgphoto2.
@@ -52,7 +52,7 @@ public class GPhoto2 {
     private String message;
     
     /**
-     * This will be called when libgphoto2 has an message to share.
+     * This will be called when libgphoto2 has an error to share.
      */
     private GPContextErrorFunc errorFunc = new GPContextErrorFunc() {
         @Override
@@ -78,7 +78,7 @@ public class GPhoto2 {
     };
         
     /**
-     * Constructor.  Loads the native libgphoto2 library and initializes it.
+     * Constructor.  Loads the native libgphoto2.so.2 library and initializes it.
      */
     public GPhoto2() {
         
@@ -93,9 +93,9 @@ public class GPhoto2 {
     }    
     
     /**
-     * Open camera connection.  Assumes only 1 camera is connected.  If multiple
+     * Open camera connection.  If multiple
      * cameras are connected only the first detected camera is used. 
-     * @throws IOException If camera cannot be opened.  Includes ligphoto2 error code.
+     * @throws IOException If camera cannot be opened.
      */
     public void open() throws IOException {                
         int result = gphoto2.gp_camera_init(camera, context);
@@ -113,96 +113,7 @@ public class GPhoto2 {
         gphoto2.gp_context_unref(context);                                
         //gphoto2.gp_camera_unref(camera);                
     }
-    
-    /**
-     * Wait (block) until the specified event is received or a timeout occurs.
-     * @param timeout Timeout value, in milliseconds
-     * @param event expected CameraEventType
-     * @throws IOException if Timeout occurs or camera is unreachable.
-     */
-    private void waitForEvent(int timeout, int event) throws IOException {   
-        IntByReference i = new IntByReference();
-        PointerByReference data = new PointerByReference();        
-        int rc;
-
-        /*
-         * need to loop, othewise GP_EVENT_UNKNOWN is almost always returned
-         */
-        while (true) {
-            rc = gphoto2.gp_camera_wait_for_event(camera, timeout, i, data, context);
-            if (rc != Gphoto2Library.GP_OK) {                
-                throw new IOException("Wait for Event failed with code " + rc);
-            }
-            if (i.getValue() == event) {
-                return;
-            } else if (i.getValue() == CameraEventType.GP_EVENT_TIMEOUT) {
-                throw new IOException("Timeout occured waiting for event " + event);
-            }
-        }                                        
-    }
-    
-    /**
-     * Capture and Image and create a CameraFilePath that points to the new
-     * image on the camera.
-     * @return CameraFilePath
-     * @throws IOException If image cannot be captured.
-     */
-    private CameraFilePath captureImage() throws IOException {        
-        CameraFilePath cameraFilePath = new CameraFilePath();        
-        int result = gphoto2.gp_camera_capture(camera, CameraCaptureType.GP_CAPTURE_IMAGE, cameraFilePath, context);
-        if (result != Gphoto2Library.GP_OK) {
-            throw new IOException(error + "(" + result + ")");
-        }                
-        waitForEvent(5000, CameraEventType.GP_EVENT_CAPTURE_COMPLETE);
-        return cameraFilePath;
-    }
-    
-    /**
-     * Save image to disk in current directory.  TODO:  allow path and
-     * filename to be specified.
-     * @param path CameraFilePath object returned by captureImage()
-     * @return a File which points to the new image.     
-     * @throws IOException If the image cannot be saved.
-     */
-    private File saveImage(CameraFilePath path) throws IOException {        
-        String folder = new String(path.folder);
-        String name = new String(path.name);  
-        int rc;
         
-        /* initialize a CameraFile object */
-        PointerByReference ref = new PointerByReference();
-        rc = gphoto2.gp_file_new(ref);
-        if (rc != Gphoto2Library.GP_OK) {
-            throw new IOException("gp_file_new failed with code " + rc);
-        }
-        CameraFile cameraFile = new CameraFile(ref.getValue());
-        
-        /* point the CameraFile object at the CameraFilePath */
-        rc = gphoto2.gp_camera_file_get(camera, folder, name, CameraFileType.GP_FILE_TYPE_NORMAL, cameraFile, context);        
-        if (rc != Gphoto2Library.GP_OK) {
-            throw new IOException("gp_camera_file_get failed with code " + rc);
-        }        
-        
-        /* save CameraFile to disk */
-        rc = gphoto2.gp_file_save(cameraFile, name);
-        gphoto2.gp_file_free(cameraFile);
-        if (rc != Gphoto2Library.GP_OK) {
-            throw new IOException("gp_file_save failed with code " + rc);
-        }
-        return new File(name);        
-    }
-    
-    /**
-     * Take a picture and save it to disk.  Currently images can only
-     * be saved into the current working directory.
-     * @return a File object which points to the saved image.  
-     * @throws IOException If image cannot be captured or saved.
-     */
-    public File captureAndDownload() throws IOException {
-        CameraFilePath path = captureImage();
-        return saveImage(path);
-    }
-    
     /**
      * Take a picture.
      * @return Name of image file on the camera.
@@ -211,7 +122,31 @@ public class GPhoto2 {
     public String capture() throws IOException {
         CameraFilePath path = captureImage();        
         return path.name.toString();
-    }            
+    }       
+    
+    /**
+     * Take a picture and save it to disk.  Currently images can only
+     * be saved into the current working directory.
+     * @param delete True if image should be removed from camera after saving.
+     * @return a File object which points to the saved image.  
+     * @throws IOException If image cannot be captured or saved.
+     */
+    public File captureAndDownload(Boolean delete) throws IOException {
+        CameraFilePath path = captureImage();
+        return saveImage(path, delete);
+    }
+    
+    /**
+     * Fire the shutter, then download all images on the camera.  Useful
+     * when using burstmode, which may capture multiple images for one
+     * captureImage() call.
+     * @param delete True if *all* images on the camera should be removed after saving.
+     * @return an ArrayList of File objects representing the saved images.
+     * @throws IOException If images cannot be captured or saved.
+     */
+    public ArrayList<File> burstAndDownload(Boolean delete) throws IOException {                                                              
+        return saveImages(captureImage(), delete);
+    }
     
     /**
      * Change a single camera setting.  If updating
@@ -243,5 +178,130 @@ public class GPhoto2 {
         String value = config.getParameter(param);        
         return value;
     }
+    
+    /**
+     * Wait (block) until the specified event is received or a timeout occurs.
+     * @param timeout Timeout value, in milliseconds
+     * @param event expected CameraEventType
+     * @throws IOException if Timeout occurs or camera is unreachable.
+     */
+    private void waitForEvent(int timeout, int event) throws IOException {   
+        IntByReference i = new IntByReference();
+        PointerByReference data = new PointerByReference();        
+        int rc;
+
+        /*
+         * need to loop, othewise GP_EVENT_UNKNOWN is almost always returned
+         */
+        while (true) {
+            rc = gphoto2.gp_camera_wait_for_event(camera, timeout, i, data, context);
+            if (rc != Gphoto2Library.GP_OK) {                
+                throw new IOException("Wait for Event failed with code " + rc);
+            }
+            if (i.getValue() == event) {
+                return;
+            } else if (i.getValue() == CameraEventType.GP_EVENT_TIMEOUT) {
+                throw new IOException("Timeout occured waiting for event " + event);
+            }
+        }                                        
+    }
+    
+    /**
+     * Capture an Image.
+     * @return CameraFilePath which references the captured image.
+     * @throws IOException If image cannot be captured.
+     */
+    private CameraFilePath captureImage() throws IOException {        
+        CameraFilePath cameraFilePath = new CameraFilePath();        
+        int result = gphoto2.gp_camera_capture(camera, CameraCaptureType.GP_CAPTURE_IMAGE, cameraFilePath, context);
+        if (result != Gphoto2Library.GP_OK) {
+            throw new IOException(error + "(" + result + ")");
+        }                
+        waitForEvent(5000, CameraEventType.GP_EVENT_CAPTURE_COMPLETE);
+        return cameraFilePath;
+    }
+    
+    /**
+     * Save image to disk in current directory.  TODO:  allow path and
+     * filename to be specified.
+     * @param path CameraFilePath object returned by captureImage()
+     * @param delete True if the image should be deleted from the camera
+     * @return a File which points to the new image.     
+     * @throws IOException If the image cannot be saved.
+     */
+    private File saveImage(CameraFilePath path, Boolean delete) throws IOException {        
+        String folder = new String(path.folder);
+        String name = new String(path.name);  
+        int rc;
+        
+        /* initialize a CameraFile object */
+        PointerByReference ref = new PointerByReference();
+        rc = gphoto2.gp_file_new(ref);
+        if (rc != Gphoto2Library.GP_OK) {
+            throw new IOException("gp_file_new failed with code " + rc);
+        }
+        CameraFile cameraFile = new CameraFile(ref.getValue());
+        
+        /* point the CameraFile object at the CameraFilePath */
+        rc = gphoto2.gp_camera_file_get(camera, folder, name, CameraFileType.GP_FILE_TYPE_NORMAL, cameraFile, context);        
+        if (rc != Gphoto2Library.GP_OK) {
+            throw new IOException("gp_camera_file_get failed with code " + rc);
+        }        
+        
+        /* save CameraFile to disk */
+        rc = gphoto2.gp_file_save(cameraFile, name);
+        gphoto2.gp_file_free(cameraFile);
+        if (rc != Gphoto2Library.GP_OK) {
+            throw new IOException("gp_file_save failed with code " + rc);
+        }
+        
+        if (delete) {
+            rc = gphoto2.gp_camera_file_delete(camera, folder, name, context);
+            if (rc != Gphoto2Library.GP_OK) {
+                throw new IOException("gp_camera_file_delete failed with code " + rc);
+            }
+        }
+        return new File(name);        
+    }
+    
+        /**
+     * Download all images in the given path into the current directory.  Warning: 
+     * If delete option is enabled, all images on the camera will be deleted, not
+     * just the ones from the most recent capture.
+     * @param path CameraFilePath to the image on the camera
+     * @param delete True if all images should be removed from camera after saving
+     * @return ArrayList of Files for the downloaded images
+     * @throws IOException on error
+     */
+    private ArrayList<File> saveImages(CameraFilePath path, Boolean delete) throws IOException {
+        ArrayList<File> fileList = new ArrayList<>();
+        String folder = new String(path.folder);
+        
+        /* get a list of files */
+        PointerByReference ref = new PointerByReference();
+        int rc = gphoto2.gp_list_new(ref);        
+        if (rc != Gphoto2Library.GP_OK) {
+            throw new IOException("gp_list_new failed with code " + rc);
+        }
+        CameraList cameraList = new CameraList(ref.getValue());
+        rc = gphoto2.gp_camera_folder_list_files(camera, folder, cameraList, context);                       
+        if (rc != Gphoto2Library.GP_OK) {
+            throw new IOException("gp_camera_folder_list_files failed with code " + rc);
+        }
+        
+        /* iterate through list, downloading each item */        
+        int gp_list_count = gphoto2.gp_list_count(cameraList);        
+        for (int i=0; i< gp_list_count; i++) {             
+            rc = gphoto2.gp_list_get_name(cameraList, i, ref);
+            if (rc != Gphoto2Library.GP_OK) {
+                throw new IOException("gp_list_get_name failed with code " + rc);
+            }
+            String name = ref.getValue().getString(0);
+            path.name = name.getBytes();
+            fileList.add(saveImage(path, delete));
+        }        
+        return fileList;        
+    }
+
                 
 }
