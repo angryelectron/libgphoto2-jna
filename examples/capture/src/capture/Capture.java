e77bc93eeebf69ba080a51ccd321aa4cdac56aaa
abythell <abythell@ieee.org>
2013-04-11 15:56:23 -0700

    add burst capture example

---
+++ examples/capture/src/capture/Capture.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capture;
+
+import com.angryelectron.gphoto2.GPhoto2;
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ZIPTREK\abythell
+ */
+public class Capture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+         * Connect to camera
+         */
+        GPhoto2 camera = new GPhoto2();
+        try {
+            camera.open();
+        } catch (IOException ex) {
+            Logger.getLogger(Capture.class.getName()).log(Level.SEVERE, ex.getMessage());
+            System.exit(-1);
+        }
+        
+        /*
+         * Capture image and download.  Nikon cameras need to buffer burst shots
+         * via the memory card, not internal memory like Canons.
+         */               
+        try {
+            camera.setConfig("burstnumber", "5");
+            camera.setConfig("capturetarget", "Memory card");
+            camera.burstAndDownload(true);
+        } catch (IOException ex) {
+            Logger.getLogger(Capture.class.getName()).log(Level.SEVERE, ex.getMessage());
+            camera.close();
+            System.exit(-1);
+        }                                     
+        
+        /*
+         * Cleanup
+         */
+        camera.close();
+    }
+}
